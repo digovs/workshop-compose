dfafa086a94a81e06ba51d696eb2478ebc1aa049
Rodrigo Silva <digovs@gmail.com>
2021-02-17 09:55:16 -0500

    Replace comments with an actual example

--- app/src/main/java/com/example/workshopcompose/MainActivity.kt
+++ app/src/main/java/com/example/workshopcompose/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.workshopcompose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.workshopcompose.ui.theme.WorkshopComposeTheme
 
@@ -26,29 +29,46 @@ fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
         Scaffold(
             topBar = {
-                // define your composable TopBar. Look for the TopAppBar component
+                TopBar()
             },
             bodyContent = {
-                /*
-                Create the body of the Form here
-                Use Column combined with TextField or OutlinedTextField and Spacer as a separator
-
-                Use States to get/set values from the input fields
-                check documentation: https://developer.android.com/codelabs/jetpack-compose-state#5
-                Example:
-                val emailState = remember { mutableStateOf(String()) }
-                OutlinedTextField(
-                    value = emailState.value,
-                    onValueChange = {
-                        emailState.value = it
-                    }
-                )
-                *
-                * */
+                BodyContent()
             }
         )
 
     }
+}
+
+@Composable
+fun TopBar() {
+    // define your composable TopBar. Look for the TopAppBar component
+}
+
+@Composable
+fun BodyContent() {
+    // Example
+    Column(modifier = Modifier.fillMaxWidth()) {
+        val emailState = remember { mutableStateOf(String()) }
+        OutlinedTextField(
+            label = {
+                Text(
+                    text = "Email",
+                    style = MaterialTheme.typography.body2
+                )
+            },
+            value = emailState.value,
+            onValueChange = {
+                emailState.value = it
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+    /*
+    Use States to get/set values from the input fields
+    check documentation: https://developer.android.com/codelabs/jetpack-compose-state#5
+    for other components: https://foso.github.io/Jetpack-Compose-Playground
+    *
+    * */
 }
 
 @Preview(showBackground = true)
